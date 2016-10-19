2d9baee3637544c9a3c248c188af5a7d81a19908
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-10-19 16:43:35 -0700

    Writing The UI for The Voting Screen

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+import Voting from './components/Voting';
+
+const pair = ['Trainspotting', '28 Days Later'];
+
+
+ReactDOM.render(
+  <Voting pair={pair} winner="Trainspotting" />,
+  document.getElementById('root')
+);
